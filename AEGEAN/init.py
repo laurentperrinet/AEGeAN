266bf1a89e0210dd65698e9c4d7561fb9a259183
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-08-04 10:48:36 +0200

    exploiting AEGEAN framework

--- AEGEAN/init.py
+++ AEGEAN/init.py
@@ -11,7 +11,7 @@ def init():
     parser.add_argument("--lrE", type=float, default=0.00015, help="adam: learning rate for E")
     parser.add_argument("--lrD", type=float, default=0.00005, help="adam: learning rate for D")
     parser.add_argument("--lrG", type=float, default=0.00005, help="adam: learning rate for G")
-    parser.add_argument("--bn_eps", type=float, default=1e-05, help="batchnorm: espilon for numerical stability")
+    parser.add_argument("--bn_eps", type=float, default=2e-01, help="batchnorm: espilon for numerical stability")
     parser.add_argument("--bn_momentum", type=float, default=.1, help="batchnorm: momentum for numerical stability")
     parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
     parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
@@ -26,10 +26,10 @@ def init():
     parser.add_argument("--latent_dim", type=int, default=32, help="dimensionality of the latent space")
     parser.add_argument("-i", "--img_size", type=int, default=128, help="size of each image dimension")
     parser.add_argument("--channels", type=int, default=3, help="number of image channels")
-    parser.add_argument("-s", "--sample_interval", type=int, default=10, help="interval between image sampling")
+    parser.add_argument("-s", "--sample_interval", type=int, default=2, help="interval between image sampling")
     parser.add_argument("--N_samples", type=int, default=25, help="number of samples to generate")
     parser.add_argument("--sample_path", type=str, default='images')
-    parser.add_argument("-m", "--model_save_interval", type=int, default=150,
+    parser.add_argument("-m", "--model_save_interval", type=int, default=5000,
                         help="interval between image sampling. If model_save_interval > n_epochs : no save")
     parser.add_argument('--model_save_path', type=str, default='models')
     parser.add_argument('--datapath', type=str, default='../../cropped_clear/cp/')
