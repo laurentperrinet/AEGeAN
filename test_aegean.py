574287c5f7dd23bc80f08752a21cafd51e554bc7
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-09-18 16:06:50 +0200

    re-run AEGeAN

--- test_aegean.py
+++ test_aegean.py
@@ -3,68 +3,68 @@ import AEGEAN as AG
 
 tag = 'test_'
 
-# # VANILLA
-# opt = AG.init()
-# opt.runs_path = tag + 'vanilla'
-# print(opt)
-# AG.learn(opt)
-#
-# # what's the effect of a smaller latent_dim ?
-# opt = AG.init()
-# opt.latent_dim, opt.runs_path = 4, tag + 'small_latent'
-# AG.learn(opt)
-#
-# # what's the effect of a smaller latent_dim ?
-# opt = AG.init()
-# opt.latent_dim, opt.runs_path = 54, tag + 'large_latent'
-# AG.learn(opt)
-#
-# opt = AG.init()
-# opt.runs_path = tag + 'small_eps'
-# opt.bn_eps = 1e-10
-# AG.learn(opt)
-#
-# opt = AG.init()
-# opt.runs_path = tag + 'big_eps'
-# opt.bn_eps = 1e-2
-# AG.learn(opt)
-#
-# opt = AG.init()
-# opt.runs_path = tag + 'small_momentum'
-# opt.bn_momentum = .01
-# AG.learn(opt)
-#
-# opt = AG.init()
-# opt.runs_path = tag + 'big_momentum'
-# opt.bn_momentum = .5
-# AG.learn(opt)
-
+# VANILLA
 opt = AG.init()
-opt.runs_path = tag + 'small_channel0'
-opt.channel0 //= 2
+opt.runs_path = tag + 'vanilla'
+print(opt)
+AG.learn(opt)
+
+# what's the effect of a smaller latent_dim ?
+opt = AG.init()
+opt.latent_dim, opt.runs_path = 4, tag + 'small_latent'
+AG.learn(opt)
+
+# what's the effect of a smaller latent_dim ?
+opt = AG.init()
+opt.latent_dim, opt.runs_path = 54, tag + 'large_latent'
 AG.learn(opt)
 
 opt = AG.init()
-opt.runs_path = tag + 'big_channel0'
-opt.channel0 *= 2
+opt.runs_path = tag + 'small_eps'
+opt.bn_eps = 1e-10
 AG.learn(opt)
 
 opt = AG.init()
-opt.runs_path = tag + 'small_channel1'
-opt.channel1 //= 2
+opt.runs_path = tag + 'big_eps'
+opt.bn_eps = 1e-2
 AG.learn(opt)
 
 opt = AG.init()
-opt.runs_path = tag + 'big_channel1'
-opt.channel1 *= 2
+opt.runs_path = tag + 'small_momentum'
+opt.bn_momentum = .01
 AG.learn(opt)
 
 opt = AG.init()
-opt.runs_path = tag + 'small_channel2'
-opt.channel2 //= 2
+opt.runs_path = tag + 'big_momentum'
+opt.bn_momentum = .5
 AG.learn(opt)
 
-opt = AG.init()
-opt.runs_path = tag + 'big_channel2'
-opt.channel2 *= 2
-AG.learn(opt)
+# opt = AG.init()
+# opt.runs_path = tag + 'small_channel0'
+# opt.channel0 //= 2
+# AG.learn(opt)
+#
+# opt = AG.init()
+# opt.runs_path = tag + 'big_channel0'
+# opt.channel0 *= 2
+# AG.learn(opt)
+#
+# opt = AG.init()
+# opt.runs_path = tag + 'small_channel1'
+# opt.channel1 //= 2
+# AG.learn(opt)
+#
+# opt = AG.init()
+# opt.runs_path = tag + 'big_channel1'
+# opt.channel1 *= 2
+# AG.learn(opt)
+#
+# opt = AG.init()
+# opt.runs_path = tag + 'small_channel2'
+# opt.channel2 //= 2
+# AG.learn(opt)
+#
+# opt = AG.init()
+# opt.runs_path = tag + 'big_channel2'
+# opt.channel2 *= 2
+# AG.learn(opt)
