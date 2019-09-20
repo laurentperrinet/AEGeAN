e6ee4c923f250a0c774a03ecc502e5e3c8f1b70d
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-09-20 14:33:17 +0200

    noise

--- AEGEAN/models.py
+++ AEGEAN/models.py
@@ -191,7 +191,12 @@ class Discriminator(nn.Module):
             print("D")
             print("Image shape : ",img.shape)
             # Dim : (opt.chanels, opt.img_size, opt.img_size)
+
         out = img + self.opt.D_noise * torch.randn(img.shape)
+        if self.opt.D_noise>0:
+            n = torch.randn(img.shape)
+            if cuda: n =n.to('cuda')
+            out += n
         if self.opt.do_whitening:
             out = conv2d(out, KW, padding=1)
 
