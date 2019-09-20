4c4b01ad8395798f4aa7790e130741266af97cf4
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-09-20 13:32:23 +0200

    re - meso

--- test_aegean.py
+++ test_aegean.py
@@ -19,68 +19,6 @@ opt.latent_dim, opt.runs_path = 54, tag + 'large_latent'
 AG.learn(opt)
 
 
-for G_loss in ['wasserstein', 'ian', 'alternative', 'original']:
-    opt = AG.init()
-    opt.runs_path = tag + 'G_loss_' + G_loss
-    opt.G_loss = G_loss
-    AG.learn(opt)
-
-opt = AG.init()
-opt.runs_path = tag + 'do_whitening'
-opt.do_whitening = True
-AG.learn(opt)
-
-opt = AG.init()
-opt.runs_path = tag + 'D_noise'
-opt.D_noise = 0.5
-AG.learn(opt)
-
-opt = AG.init()
-opt.runs_path = tag + 'no_affine'
-opt.rand_affine = 0.
-AG.learn(opt)
-
-opt = AG.init()
-opt.runs_path = tag + 'no_bn'
-opt.bn_eps = np.inf
-AG.learn(opt)
-
-opt = AG.init()
-opt.runs_path = tag + 'relu'
-opt.lrelu = 0.
-AG.learn(opt)
-
-opt = AG.init()
-opt.runs_path = tag + 'valid_smooth'
-opt.valid_smooth = 0.9
-AG.learn(opt)
-
-# VANILLA
-opt = AG.init()
-opt.runs_path = tag + 'vanilla'
-print(opt)
-AG.learn(opt)
-
-opt = AG.init()
-opt.runs_path = tag + 'small_eps'
-opt.bn_eps = 1e-10
-AG.learn(opt)
-
-opt = AG.init()
-opt.runs_path = tag + 'big_eps'
-opt.bn_eps = 1e-2
-AG.learn(opt)
-
-opt = AG.init()
-opt.runs_path = tag + 'small_momentum'
-opt.bn_momentum = .01
-AG.learn(opt)
-
-opt = AG.init()
-opt.runs_path = tag + 'big_momentum'
-opt.bn_momentum = .5
-AG.learn(opt)
-
 opt = AG.init()
 opt.runs_path = tag + 'small_channel0'
 opt.channel0 //= 2
@@ -109,4 +47,69 @@ AG.learn(opt)
 opt = AG.init()
 opt.runs_path = tag + 'big_channel2'
 opt.channel2 *= 2
+AG.learn(opt)
+
+
+for G_loss in ['wasserstein', 'ian', 'alternative', 'original']:
+    opt = AG.init()
+    opt.runs_path = tag + 'G_loss_' + G_loss
+    opt.G_loss = G_loss
+    AG.learn(opt)
+
+opt = AG.init()
+opt.runs_path = tag + 'do_whitening'
+opt.do_whitening = True
+AG.learn(opt)
+
+opt = AG.init()
+opt.runs_path = tag + 'D_noise'
+opt.D_noise = 0.5
+AG.learn(opt)
+
+opt = AG.init()
+opt.runs_path = tag + 'no_affine'
+opt.rand_affine = 0.
+AG.learn(opt)
+
+
+opt = AG.init()
+opt.runs_path = tag + 'relu'
+opt.lrelu = 0.
+AG.learn(opt)
+
+opt = AG.init()
+opt.runs_path = tag + 'valid_smooth'
+opt.valid_smooth = 0.9
+AG.learn(opt)
+
+# VANILLA
+opt = AG.init()
+opt.runs_path = tag + 'vanilla'
+print(opt)
+AG.learn(opt)
+
+
+opt = AG.init()
+opt.runs_path = tag + 'no_bn'
+opt.bn_eps = np.inf
+AG.learn(opt)
+
+opt = AG.init()
+opt.runs_path = tag + 'small_eps'
+opt.bn_eps = 1e-10
+AG.learn(opt)
+
+opt = AG.init()
+opt.runs_path = tag + 'big_eps'
+opt.bn_eps = 1e-2
+AG.learn(opt)
+
+opt = AG.init()
+opt.runs_path = tag + 'small_momentum'
+opt.bn_momentum = .01
+AG.learn(opt)
+
+opt = AG.init()
+opt.runs_path = tag + 'big_momentum'
+opt.bn_momentum = .5
 AG.learn(opt)
