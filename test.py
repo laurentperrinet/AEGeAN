e487fa59aa4305d881b676831262aa4ef75c1dc8
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-10-17 14:52:07 +0200

    Update test.py

--- test.py
+++ test.py
@@ -15,126 +15,128 @@ for expname in experiments.keys():
         for variable, value in experiments[expname]:
             vars(opt)[variable] = value
         tag = f'{expname}_{opt.img_size}_'
-        # print(tag, opt)
+        return tag, opt
+
 
     # VANILLA
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'vanilla'
     print(opt)
     AG.learn(opt)
-
     GAN_losses = ['original', 'wasserstein', 'ian', 'alternative'] #, 'alternativ2'
 
     for GAN_loss in GAN_losses:
-        opt = init()
-        opt.runs_path = tag + 'GAN_loss_' + GAN_loss
-        opt.GAN_loss = GAN_loss
-        AG.learn(opt)
+        tag, opt = init()
+        if opt.lrD > 0:
+            opt.runs_path = tag + 'GAN_loss_' + GAN_loss
+            opt.GAN_loss = GAN_loss
+            AG.learn(opt)
 
     for GAN_loss in GAN_losses:
-        opt = init()
-        opt.runs_path = tag + 'GAN_loss_' + GAN_loss
-        opt.GAN_loss = GAN_loss
-        opt.runs_path += '_no_bn'
-        opt.bn_eps = np.inf
-        AG.learn(opt)
+        tag, opt = init()
+        if opt.lrD > 0:
+            opt.runs_path = tag + 'GAN_loss_' + GAN_loss
+            opt.GAN_loss = GAN_loss
+            opt.runs_path += '_no_bn'
+            opt.bn_eps = np.inf
+            AG.learn(opt)
 
 
     # what's the effect of a smaller latent_dim ?
-    opt = init()
+    tag, opt = init()
     opt.latent_dim, opt.runs_path = opt.latent_dim//2, tag + 'small_latent'
     AG.learn(opt)
 
     # what's the effect of a smaller latent_dim ?
-    opt = init()
+    tag, opt = init()
     opt.latent_dim, opt.runs_path = opt.latent_dim*2, tag + 'large_latent'
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'small_lrE'
     opt.lrE /= base
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'big_lrE'
     opt.lrE *= base
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     if opt.lrD > 0:
         opt.runs_path = tag + 'small_lrD'
         opt.lrD /= base
         AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     if opt.lrD > 0:
         opt.runs_path = tag + 'big_lrD'
         opt.lrD *= base
         AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     if opt.lrD > 0:
         opt.runs_path = tag + 'small_lrG'
         opt.lrG /= base
         AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     if opt.lrD > 0:
         opt.runs_path = tag + 'big_lrG'
         opt.lrG *= base
         AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'small_lambdaE'
     opt.lambdaE /= base
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'big_lambdaE'
     opt.lambdaE *= base
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'small_channel0'
     opt.channel0 //= base
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'big_channel0'
     opt.channel0 *= base
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'small_channel1'
     opt.channel1 //= base
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'big_channel1'
     opt.channel1 *= base
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'small_channel2'
     opt.channel2 //= base
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'big_channel2'
     opt.channel2 *= base
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'low_D_noise'
     opt.D_noise /= base
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'high_D_noise'
     opt.D_noise *= base
     AG.learn(opt)
 
-    # opt = init()
+    # tag, opt = init()
     # if opt.do_whitening:
     #     opt.runs_path = tag + 'no_whitening'
     # else:
@@ -142,7 +144,7 @@ for expname in experiments.keys():
     # opt.do_whitening = not opt.do_whitening
     # AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.init_weight = not opt.init_weight
     if opt.init_weight:
         opt.runs_path = tag + 'do_init_weight'
@@ -150,7 +152,7 @@ for expname in experiments.keys():
         opt.runs_path = tag + 'no_init_weight'
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.do_bias = not opt.do_bias
     if opt.do_bias:
         opt.runs_path = tag + 'do_bias'
@@ -159,59 +161,59 @@ for expname in experiments.keys():
     AG.learn(opt)
 
     if False:
-        opt = init()
+        tag, opt = init()
         opt.runs_path = tag + 'low_batch_size'
         opt.batch_size //= base
         AG.learn(opt)
 
-        opt = init()
+        tag, opt = init()
         opt.runs_path = tag + 'high_batch_size'
         opt.batch_size *= base
         AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'low_adam_beta1'
     opt.beta1 = 0.2
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'high_adam_beta1'
     opt.beta1 = 0.9
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'low_adam_beta2'
     opt.beta2 = 0.9
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'high_adam_beta2'
     opt.beta2 = 0.99
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'no_affine'
     opt.rand_affine = 0.
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'relu' if opt.lrelu==0. else tag + 'lrelu'
     opt.lrelu = 0.02 if opt.lrelu==0. else 0.
     AG.learn(opt)
 
 
-    opt = init()
+    tag, opt = init()
     if opt.lrD > 0:
         opt.runs_path = tag + 'low_valid_smooth'
         opt.valid_smooth = 0.9
         AG.learn(opt)
-    opt = init()
+    tag, opt = init()
     if opt.lrD > 0:
         opt.runs_path = tag + 'no_valid_smooth'
         opt.valid_smooth = 1.
         AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.do_joint = not opt.do_joint
     if opt.do_joint:
         opt.runs_path = tag + 'do_joint'
@@ -220,7 +222,7 @@ for expname in experiments.keys():
     AG.learn(opt)
 
 
-    opt = init()
+    tag, opt = init()
     if opt.bn_eps == np.inf:
         opt.runs_path = tag + 'do_bn'
         opt.bn_eps = .3
@@ -229,22 +231,22 @@ for expname in experiments.keys():
         opt.bn_eps = np.inf
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'small_bn_eps'
     opt.bn_eps /= base
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'big_bn_eps'
     opt.bn_eps *= base
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'small_bn_momentum'
     opt.bn_momentum = .3
     AG.learn(opt)
 
-    opt = init()
+    tag, opt = init()
     opt.runs_path = tag + 'big_bn_momentum'
     opt.bn_momentum = .9
     AG.learn(opt)
