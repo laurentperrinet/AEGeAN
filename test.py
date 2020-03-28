f0c55402650a08c2854329657a1e38375cc3cde6
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2020-03-28 08:51:07 +0100

    more tests

--- test.py
+++ test.py
@@ -8,8 +8,8 @@ import numpy as np
 
 experiments = {}
 experiments['AEGEAN_64'] = [('img_size', 64)]# , ('n_epochs', 512)]
-# experiments['Simpsons_64'] = [('datapath', '../database/Simpsons-Face_clear/cp/'), ('img_size', 64), ('do_SSIM', False)]
-# experiments['AEGEAN_128'] = [('img_size', 128), ('n_epochs', 512)]
+experiments['Simpsons_64'] = [('datapath', '../database/Simpsons-Face_clear/cp/'), ('img_size', 64), ('do_SSIM', False)]
+experiments['AEGEAN_128'] = [('img_size', 128), ('n_epochs', 512)]
 # experiments['AEGEAN_256'] = [('img_size', 256), ]
 # experiments['Simpsons_256'] = [('datapath', '../database/Simpsons-Face_clear/cp/'), ('img_size', 256), ('do_SSIM', False)]
 # experiments['AEGEAN_128'] = [('img_size', 128), ]
