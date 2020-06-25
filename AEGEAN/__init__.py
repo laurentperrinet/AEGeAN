d1ec338f909d6ca81de013e6bf5d40270787d34f
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2020-06-25 15:42:22 +0200

    Update __init__.py

--- AEGEAN/__init__.py
+++ AEGEAN/__init__.py
@@ -6,11 +6,11 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 import matplotlib
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 from .init import *
 from .models import *
 from .utils import *
 from .aegean import *
-import matplotlib.pyplot as plt
 
 __all__ = ["utils", "models", "init", "aegean"]
