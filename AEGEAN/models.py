c659c079256589c352fb69d78e1bf0167cefbab2
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2020-10-09 14:19:31 +0200

    Update models.py

--- AEGEAN/models.py
+++ AEGEAN/models.py
@@ -8,6 +8,41 @@ import torch
 
 cuda = True if torch.cuda.is_available() else False
 
+# norm_layer = nn.BatchNorm2d
+# https://pytorch.org/docs/stable/generated/torch.nn.InstanceNorm2d.html#instancenorm2d
+norm_layer = nn.InstanceNorm2d
+class ResBlock(nn.Module):
+    def __init__(self, f):
+        super(ResBlock, self).__init__()
+        self.conv = nn.Sequential(nn.Conv2d(f, f, 3, 1, 1),
+                                  norm_layer(f),
+                                  nn.ReLU(),
+                                  nn.Conv2d(f, f, 3, 1, 1))
+        self.norm = norm_layer(f)
+    def forward(self, x):
+        return F.relu(self.norm(self.conv(x)+x))
+
+# class Generator(nn.Module):
+#     def __init__(self, f=64, blocks=6):
+#         super(Generator, self).__init__()
+#         layers = [nn.ReflectionPad2d(3),
+#                   nn.Conv2d(  3,   f, 7, 1, 0), norm_layer(  f), nn.ReLU(True),
+#                   nn.Conv2d(  f, 2*f, 3, 2, 1), norm_layer(2*f), nn.ReLU(True),
+#                   nn.Conv2d(2*f, 4*f, 3, 2, 1), norm_layer(4*f), nn.ReLU(True)]
+#         for i in range(int(blocks)):
+#             layers.append(ResBlock(4*f))
+#         layers.extend([
+#                 nn.ConvTranspose2d(4*f, 4*2*f, 3, 1, 1), nn.PixelShuffle(2), norm_layer(2*f), nn.ReLU(True),
+#                 nn.ConvTranspose2d(2*f,   4*f, 3, 1, 1), nn.PixelShuffle(2), norm_layer(  f), nn.ReLU(True),
+#                 nn.ReflectionPad2d(3), nn.Conv2d(f, 3, 7, 1, 0),
+#                 nn.Tanh()])
+#         self.conv = nn.Sequential(*layers)
+#
+#     def forward(self, x):
+#         return self.conv(x)
+#
+# see also https://towardsdatascience.com/simpsonize-yourself-using-cyclegan-and-pytorch-cea94ee199ca
+
 class Encoder(nn.Module):
     def __init__(self, opt):
         super(Encoder, self).__init__()
@@ -75,40 +110,6 @@ class Encoder(nn.Module):
     def _name(self):
         return "Encoder"
 
-# norm_layer = nn.BatchNorm2d
-# https://pytorch.org/docs/stable/generated/torch.nn.InstanceNorm2d.html#instancenorm2d
-norm_layer = nn.InstanceNorm2d
-class ResBlock(nn.Module):
-    def __init__(self, f):
-        super(ResBlock, self).__init__()
-        self.conv = nn.Sequential(nn.Conv2d(f, f, 3, 1, 1),
-                                  norm_layer(f),
-                                  nn.ReLU(),
-                                  nn.Conv2d(f, f, 3, 1, 1))
-        self.norm = norm_layer(f)
-    def forward(self, x):
-        return F.relu(self.norm(self.conv(x)+x))
-
-# class Generator(nn.Module):
-#     def __init__(self, f=64, blocks=6):
-#         super(Generator, self).__init__()
-#         layers = [nn.ReflectionPad2d(3),
-#                   nn.Conv2d(  3,   f, 7, 1, 0), norm_layer(  f), nn.ReLU(True),
-#                   nn.Conv2d(  f, 2*f, 3, 2, 1), norm_layer(2*f), nn.ReLU(True),
-#                   nn.Conv2d(2*f, 4*f, 3, 2, 1), norm_layer(4*f), nn.ReLU(True)]
-#         for i in range(int(blocks)):
-#             layers.append(ResBlock(4*f))
-#         layers.extend([
-#                 nn.ConvTranspose2d(4*f, 4*2*f, 3, 1, 1), nn.PixelShuffle(2), norm_layer(2*f), nn.ReLU(True),
-#                 nn.ConvTranspose2d(2*f,   4*f, 3, 1, 1), nn.PixelShuffle(2), norm_layer(  f), nn.ReLU(True),
-#                 nn.ReflectionPad2d(3), nn.Conv2d(f, 3, 7, 1, 0),
-#                 nn.Tanh()])
-#         self.conv = nn.Sequential(*layers)
-#
-#     def forward(self, x):
-#         return self.conv(x)
-#
-# see also https://towardsdatascience.com/simpsonize-yourself-using-cyclegan-and-pytorch-cea94ee199ca
 
 class Generator(nn.Module):
     def __init__(self, opt):
